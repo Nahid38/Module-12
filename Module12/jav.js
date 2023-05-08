63b80a80bdae83fce1aeadeb660edd4125b2b9b4
Nahid38 <129079402+Nahid38@users.noreply.github.com>
2023-05-09 00:15:27 +0600

    Delete Module12 directory

--- Module12/jav.js
+++
@@ -1,7 +0,0 @@
-function addListItem() {
-    const myList = document.getElementById("myList");
-    const newListItem = document.createElement("li");
-    const textNode = document.createTextNode("Apples");
-    newListItem.appendChild(textNode);
-    myList.appendChild(newListItem);
-  }
